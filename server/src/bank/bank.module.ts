2efc07a928e3b4686c482aca592091491cff4d8e
Kyrylo Denysenko <kirill.denisenko@devit.group>
2024-08-27 17:23:27 +0300

    Initial bank

---
+++ server/src/bank/bank.module.ts
@@ -0,0 +1,9 @@
+import {Module} from '@nestjs/common';
+import {BankController} from "./bank.controller";
+
+@Module({
+  controllers: [BankController],
+  providers:[]
+})
+
+export class BankModule {}
